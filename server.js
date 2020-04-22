8fd174d5cda3ae804298aab7195f021fa94978b0
Marvin Castro <bbmarvin2003@yahoo.com>
2020-04-22 16:25:48 -0400

    adding mlab code

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 var routes = require("./controller/fitness_controller.js");
-var URI = process.env.MONGODB_URI || "mongodb://fitness-app:1C2a3s45@ds221115.mlab.com:21115/heroku_rjgh246j";
+var URI = process.env.MONGODB_URI || "mongodb://fitness-app:1C2a3s45@ds221115.mlab.com:23822/heroku_cpq91z8t";
+// mongodb://heroku_cpq91z8t:2hvajco87oba9j23jbhntq0n63@ds123822.mlab.com:23822/heroku_cpq91z8t
+// mongodb://fitness-app:1C2a3s45@ds221115.mlab.com:21115/heroku_rjgh246j
 mongoose.connect(URI, {
     useCreateIndex: true,
     useNewUrlParser: true
